ab2a8656c3b6326d51c2b24e7012188149952a80
hainesma <hainesma@gmail.com>
2021-06-03 15:55:23 -0400

    Add project files.

---
+++ Guesser/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Guesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generate random number
+            Random rand = new Random();
+            int secretNumber = rand.Next(1, 101);
+            Console.WriteLine(secretNumber);
+
+            int guess = GetInteger();
+
+        }
+
+        public static int GetInteger()
+        {
+            int output = 0;
+            
+            try
+            {
+                string input = Console.ReadLine();
+                output = int.Parse(input);
+                if (output < 1 || output > 100)
+                {
+                    throw new Exception("Your guess must be between 1 and 100.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("That was not a number.");
+                output = GetInteger();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                output = GetInteger();
+            }
+            return output;
+
+        }
+
+    }
+}
