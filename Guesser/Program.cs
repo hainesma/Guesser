3bb4e9a20271fd9a4283592cf791f14d541f5136
hainesma <hainesma@gmail.com>
2021-06-03 21:54:49 -0400

    save all

--- Guesser/Program.cs
+++ Guesser/Program.cs
@@ -11,16 +11,34 @@ namespace Guesser
             int secretNumber = rand.Next(1, 101);
             Console.WriteLine(secretNumber);
 
-            int guess = GetInteger();
+            bool keepGuessing = true;
+            while (keepGuessing == true)
+            {
 
+                
+                int guess = GetInteger(secretNumber);
+                Console.WriteLine($"Your guess is {guess}.");
+
+                //if (TryGuess(secretNumber, guess) == true)
+                //{
+                //    Console.WriteLine("You guessed the correct number!");
+                //    keepGuessing = false;
+                //}
+                //else
+                //{
+                //    Console.WriteLine("That number is not correct.");
+                //    guess = GetInteger();
+                //}
+            }
         }
 
-        public static int GetInteger()
+        public static int GetInteger(int target )
         {
             int output = 0;
             
             try
             {
+                Console.WriteLine("Please enter a guess 1-100.");
                 string input = Console.ReadLine();
                 output = int.Parse(input);
                 if (output < 1 || output > 100)
@@ -31,16 +49,38 @@ namespace Guesser
             catch (FormatException)
             {
                 Console.WriteLine("That was not a number.");
-                output = GetInteger();
+                output = GetInteger(target);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                output = GetInteger();
+                output = GetInteger(target);
+            }
+
+            if (output == target)
+            {
+                Console.WriteLine("You guessed the correct number!");
+                keepGuessing = false;
+            }
+            else
+            {
+                Console.WriteLine("That number is not correct.");
+                output = GetInteger(target);
             }
             return output;
 
         }
+
+        //public static bool TryGuess(int target, int guess)
+        //{
+        //    if(guess == target)
+        //    {
+        //        return true;
+        //    } else
+        //    {
+        //        return false;
+        //    }
+        //}
 
     }
 }
